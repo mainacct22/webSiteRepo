cbd970d64ff7f4ea358ab00d4f13e824c69821fe
ChewbacaJones <granthamj001@mymail.mssu.edu>
2019-07-14 11:10:46 -0500

    trying CANVAS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   width: window.innerWidth * window.devicePixelRatio,
   height: window.innerHeight * window.devicePixelRatio,
   parent: "game-container",
