71a2a78929423f47f00eeda49a4c5f2aa96b3ee7
ChewbacaJones <granthamj001@mymail.mssu.edu>
2019-07-14 10:38:10 -0500

    scaling function

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const config = {
   height: 600,
   parent: "game-container",
   pixelArt: true,
-  scale: {
-	  mode: Phaser.Scale.ENVELOP,
-	  autoCenter: Phaser.Scale.CENTER_BOTH
-  },
   physics: {
     default: "matter",
   },
@@ -63,6 +59,13 @@ function preload() {
 }
 
 function create() {
+	/*
+	scale: {
+	  mode: Phaser.Scale.ENVELOP,
+	  autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  */
+	
 	
 	this.matter.world.setBounds(0,0, gameWidth * 18, gameHeight);
 	
@@ -238,6 +241,8 @@ function create() {
 	//console.log("camera w = " camera.width);
 
   // Debug graphics
+  window.addEventListener('resize', resizeApp);
+
 }
 
 function update(time, delta) {
@@ -271,4 +276,29 @@ function update(time, delta) {
     
     //this.controls.update(delta);
 
+}
+
+function resizeApp ()
+{
+	// Width-height-ratio of game resolution
+    // Replace 360 with your game width, and replace 640 with your game height
+	let game_ratio		= 800 / 600;
+	
+	// Make div full height of browser and keep the ratio of game resolution
+	let div			= document.getElementById('phaser-app');
+	div.style.width		= (window.innerHeight * game_ratio) + 'px';
+	div.style.height	= window.innerHeight + 'px';
+	
+	// Check if device DPI messes up the width-height-ratio
+	let canvas			= document.getElementsByTagName('canvas')[0];
+	
+	let dpi_w	= parseInt(div.style.width) / canvas.width;
+	let dpi_h	= parseInt(div.style.height) / canvas.height;		
+	
+	let height	= window.innerHeight * (dpi_w / dpi_h);
+	let width	= height * game_ratio;
+	
+	// Scale canvas	
+	canvas.style.width	= width + 'px';
+	canvas.style.height	= height + 'px';
 }
