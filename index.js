e874ac9034369e3eb1404725c1a0846c651ba744
ChewbacaJones <granthamj001@mymail.mssu.edu>
2019-07-15 12:00:43 -0500

    add cursor to debug platforms

--- index.js
+++ index.js
@@ -241,15 +241,15 @@ function create() {
   camera.setBounds(0, 0, gameWidth * 18, gameHeight);
   camera.startFollow(sandbag);
 
-  //cursors = this.input.keyboard.createCursorKeys();
-  //const controlConfig = {
-  //    camera: this.cameras.main,
-  //    left: cursors.left,
-  //    right: cursors.right,
-  //    up: cursors.up,
-  //    down: cursors.down,
-  //    speed: 0.5
-  //  };
+  cursors = this.input.keyboard.createCursorKeys();
+  const controlConfig = {
+      camera: this.cameras.main,
+      left: cursors.left,
+      right: cursors.right,
+      up: cursors.up,
+      down: cursors.down,
+      speed: 0.5
+    };
     
   //this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
 
